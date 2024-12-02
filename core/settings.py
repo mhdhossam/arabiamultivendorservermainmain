ba6b85893d36887c27862a20ed14ed430b8376bb
Mohamed Hossam <80326979+mhdhossam@users.noreply.github.com>
2024-12-02 17:46:00 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -133,24 +133,24 @@ AUTH_USER_MODEL = 'useraccount.User'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql',
-'NAME': env("DB_NAME"),
-'USER': env("DB_USER"),
-'PASSWORD': env("DB_PASSWORD"),
-'HOST': env("DB_HOST"),
-'PORT': env("DB_PORT"), 
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-}
+
+
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.postgresql',
+# 'NAME': env("DB_NAME"),
+# 'USER': env("DB_USER"),
+# 'PASSWORD': env("DB_PASSWORD"),
+# 'HOST': env("DB_HOST"),
+# 'PORT': env("DB_PORT"), 
+# }
+# }
 
 
 # Password validation
